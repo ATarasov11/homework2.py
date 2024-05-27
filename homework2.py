ca62ace09b9719cfe51e92e381ce02813e84c523
ATarasov11 <kapitan-tarasov@mail.ru>
2024-05-27 08:30:07 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
-amount_tasks=', всего задач: 12'
-number_of_hours=', затрачено часов: 1.5'
+amount_tasks=', всего задач:'
+number_of_hours=', затрачено часов: '
 name_course='Курс: '
 mean_time=', среднее время выполнения'
-print((name_course+'Python'+amount_tasks+number_of_hours+mean_time),(float(number_of_hours[-3:]))/(int(amount_tasks[-2:])),'часа.')
+print((name_course+'Python'+amount_tasks+'12'+number_of_hours+'1.5'+mean_time),(1.5/12),'часа.')
